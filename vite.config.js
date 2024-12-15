1e429a8e1c51bd6f6cce52d1d721786564a88876
joshua <zaitochegne@gmail.com>
2024-12-15 17:03:44 -0500

    se modifica el vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env // Para asegurar que solo las variables necesarias estén disponibles
+  },
+  envPrefix: 'VITE_' // Asegura que solo las variables con el prefijo VITE_ se inyecten en el frontend
 })
