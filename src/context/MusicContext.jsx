311edbf1744ebe2ea1ca870bf3ce1d157ac26ef7
joshua <zaitochegne@gmail.com>
2024-12-14 20:37:59 -0500

    se borra estados que no se usan de musicContext

--- src/context/MusicContext.jsx
+++ src/context/MusicContext.jsx
@@ -2,37 +2,31 @@
 import { createContext, useState } from "react"
 import { useWavesurfer } from "@wavesurfer/react";
 
-let urlsMusic = [
-    "eiHqkDoFFFU",
-    "dQ4XHW5Q4gU",
-    "TvZskcqdYcE"
+//let urlsMusic = [
+  //  "eiHqkDoFFFU",
+   // "dQ4XHW5Q4gU",
+  //  "TvZskcqdYcE"]
 
-]
 export let MusicContext = createContext(null);
 
 export default function MusicContextProvider({ children }) {
     const [indexSong, setIndexSong] = useState(0);
-    const [play, setPlay] = useState("play");
-    const [wavesurferMedia, setWavesurferMedia] = useState("");
-    const [urlSongs, setUrlSongs] = useState(urlsMusic)
+    //const [play, setPlay] = useState("play");
+    //const [wavesurferMedia, setWavesurferMedia] = useState("");
+    //const [urlSongs, setUrlSongs] = useState(urlsMusic)
     const [currentTime, setCurrentTime] = useState('');
     const [isMoved, setIsMoved] = useState(true);
-    const [player, setPlayer] = useState(null); // Estado para el reproductor
-    const [isPlaying, setIsPlaying] = useState(false);
+    //const [player, setPlayer] = useState(null); // Estado para el reproductor
+   // const [isPlaying, setIsPlaying] = useState(false);
     const [progress, setProgress] = useState(0);
     const [volume, setVolume] = useState(50);
-    const [resultSearchYoutube,setResultSearchYoutube] = useState([]);
-    const [musicActually,setMusicActually] = useState({});
-    const audio = new Audio();
-    audio.src = urlSongs[indexSong];
+    //const [resultSearchYoutube,setResultSearchYoutube] = useState([]);
+    //const [musicActually,setMusicActually] = useState({});
+    //const audio = new Audio();
+   // audio.src = urlSongs[indexSong];
     
-    const updatePlay = (value) => {
-        setPlay(value);
-    }
-    const updateWavesurfer = (wave) => {
-        setWavesurferMedia(wave);
-
-    }
+   
+   
     return (
         <MusicContext.Provider value={{
             volume,
