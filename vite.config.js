d1573839a8246c4f71cfe98852f8d1e87d04bd48
joshua <zaitochegne@gmail.com>
2024-12-17 17:38:06 -0500

    se configura  pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import {VitePWA} from 'vite-plugin-pwa'
 // https://vite.dev/config/
+
+const manifestForPlugin ={
+  registerType:'prompt',
+  includeAssets:['favicon.ico','apple-touch-icon.png','masked-icon.png'],
+  manifest:{
+    name:'youVideo app',
+    short_name:'youVideo',
+    description:'App that search and listen music',
+    icons:[
+      {src:'/imgpwa/android-chrome-192x192.png',sizes:'192x192',type:'image/png'},
+      {src:'/imgpwa/android-chrome-512x512.png',sizes:'512x512',type:'image/png',purpose:'favicon'},
+      {src:'/imgpwa/apple-touch-icon.png',sizes:'180x180',type:'image/png',purpose:'apple touch icon'},
+      {src:'/imgpwa/android-chrome-144x144.png',sizes:'144x144',type:'image/png',purpose:'any'},
+      {src:'/imgpwa/android-chrome-256x256.png',sizes:'256x256',type:'image/png',purpose:'icon'},
+      {src:'/imgpwa/android-chrome-384x384.png',sizes:'384x384',type:'image/png',purpose:'any masktable'}
+    ],
+    theme_color:'#FFFFFF',
+    background_color:'#FFFFFF',
+    display:'standalone',
+    scope:'/',
+    start_url:'/',
+    orientation:'portrait'
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA(manifestForPlugin)],
   define: {
-    'process.env': process.env // Para asegurar que solo las variables necesarias estén disponibles
+ 
   },
-  envPrefix: 'VITE_' // Asegura que solo las variables con el prefijo VITE_ se inyecten en el frontend
 })
