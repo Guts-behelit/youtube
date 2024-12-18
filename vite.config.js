ba141173e806e54b3a836af16d8c15cd8103a737
joshua <zaitochegne@gmail.com>
2024-12-17 19:21:17 -0500

    se cambia la imamgen de icono android   pwa a #FFFFFF

--- vite.config.js
+++ vite.config.js
@@ -12,13 +12,13 @@ const manifestForPlugin ={
     description:'App that search and listen music',
     icons:[
       {src:'/imgpwa/android-chrome-192.png',sizes:'192x192',type:'image/png'},
-      {src:'/imgpwa/android-chrome-512x512.png',sizes:'512x512',type:'image/png',purpose:'favicon'},
+      {src:'/imgpwa/512.png',sizes:'512x512',type:'image/png',purpose:'favicon'},
       {src:'/imgpwa/apple-touch-icon.png',sizes:'180x180',type:'image/png',purpose:'apple touch icon'},
-      {src:'/imgpwa/android-chrome-144x144.png',sizes:'144x144',type:'image/png',purpose:'any'},
-      {src:'/imgpwa/android-chrome-256x256.png',sizes:'256x256',type:'image/png',purpose:'icon'},
+      {src:'/imgpwa/144.png',sizes:'144x144',type:'image/png',purpose:'any'},
+      {src:'/imgpwa/256.png',sizes:'256x256',type:'image/png',purpose:'icon'},
       {src:'/imgpwa/android-chrome-384x384.png',sizes:'384x384',type:'image/png',purpose:'any masktable'}
     ],
-    theme_color:'#000000',
+    theme_color:'#FFFFFF',
     background_color:'#000000',
     display:'standalone',
     scope:'/',
